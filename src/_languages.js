d39ba34f6476dc52bc7cc61f4b4b32431e1834b5
LorenzoLeonardini <lorenzo@leonardini.dev>
2022-01-24 15:42:12 +0100

    fix

--- src/_languages.js
+++ src/_languages.js
@@ -1,1 +1,1 @@
-export default {en:{name:"English",file:"en.json",flag:"gb"},it:{name:"Italiano",file:"it.json",flag:"it"},fr:{name:"Francais",file:null,flag:"fr"}};
+export default { en: { name: "English", file: "en.json", flag: "gb" }, it: { name: "Italiano", file: "it.json", flag: "it" }, fr: { name: "Français", file: "fr.json", flag: "fr" } };
